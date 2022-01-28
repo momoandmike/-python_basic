e257ed92559d7bf7ec7b86c62e2f994eb658cbe0
momoandmike <momoandmike@gmail.com>
2022-01-29 08:22:25 +0900

    Time up

--- B-5.py
+++ B-5.py
@@ -1,15 +1,21 @@
 list = input("データを入力してください > ")
 
+scores = list(map(int, input("入力").split()))
 
+print(scores)
 # 合計値
 
-def total():
-    if len(list) < 1:
-        return len(list)
-    return len(list) + total(len(list) - 1)
+# def total():
+# if len(list) < 1:
+# return len(list)
+# return len(list) + total(len(list) - 1)
 
 # 最大値
+
+
 # 最小値
+
+
 # 平均値
 
 
